59e767a6ae75ad777a9ad37d08882fc5081c4b8d
paperbox <lego37yoon@outlook.com>
2023-06-28 23:13:48 +0900

    func: initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    
+</script>
+
+<svelte:head>
+    <title>Info DustDrive: 환영합니다</title>
+</svelte:head>
+
+<h1>Info DustDrive</h1>
+<p>노후 경유차, 아직 바꿀 수 없다면</p>
+
+<form name="inputCar">
+    <input type="text" name="carNumber" />
+    <input type="submit" value="저장하고 확인하기" />
+</form>
+
+<dialog>
+    <p>이 웹사이트는 쿠키를 사용합니다. 자동차 번호를 저장하고 계속하시려면 쿠키를 허용해주세요. 거부하신 경우, 서비스 이용 시마다 입력이 필요합니다. 이 설정은 하단 메뉴에서 변경하실 수 있습니다.</p>
+    <button>허용</button>
+    <button>거부</button>
+</dialog>
+
+<style>
+    
+</style>
