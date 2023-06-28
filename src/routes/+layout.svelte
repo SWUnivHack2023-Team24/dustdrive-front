3a5d9c848797d988b0bb04b59f6add22824b5b86
paperbox <lego37yoon@outlook.com>
2023-06-29 03:58:50 +0900

    feat: add current year to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    const currentYear = new Date().getFullYear();
+</script>
+
+<header>
+    <h1>Yellow Calendar</h1>
+</header>
+
+<slot></slot>
+
+<footer>
+    <p>Copyright by {currentYear} DustDrive Team</p>
+</footer>
+
+<style>
+    header, footer {
+        font-family: "Pretendard Variable", sans-serif;
+        text-align: center;
+    }
+
+    header {
+        height: 5vh;
+    }
+
+    footer {
+        height: 5vh;
+        width: 100%;
+        bottom: 0;
+        position: fixed;
+    }
+</style>
