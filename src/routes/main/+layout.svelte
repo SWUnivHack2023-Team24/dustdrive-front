5ac38d67b11b24afdbeb131b7ee116ad4d5c3b60
paperbox <lego37yoon@outlook.com>
2023-06-29 23:35:16 +0900

    feat(layout): add common style to layout

---
+++ src/routes/main/+layout.svelte
@@ -0,0 +1,13 @@
+
+<slot></slot>
+
+<style>
+    :global(html) {
+        height: 100%;
+
+        background: #F6F6F6;
+        padding: 1em;
+        margin: 0;
+        font-family: "Pretendard Variable", sans-serif;
+    }
+</style>
