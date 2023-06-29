813fa6f2307425e2f552b788f1e320f32f124197
paperbox <lego37yoon@outlook.com>
2023-06-29 20:03:51 +0900

    fix(loading): apply design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script>
     import { navigating } from "$app/stores";
-
-    const currentYear = new Date().getFullYear();
 </script>
 
 <slot></slot>
@@ -9,27 +7,39 @@
 {#if $navigating}
 <dialog open>
     <p>
-        <span class="primary">옐로우캘린더</span>가
-        <span class="secondary">옐로카</span> 정보를 확인하고 있어요</p>
+        <span class="secondary">옐로우캘린더</span>가<br>
+        <span class="primary">MY 옐로카 정보</span>를<br>
+        확인하고 있어요</p>
     <div class="loadingCircle" />
 </dialog>
 {/if}
 
 <style>
     dialog {
-        position: fixed;
-        top: 10vh;
-        height: 80vh;
+        font-weight: 700;
+        font-size: 1.5em;
+
+        top: 0;
+        height: 100vh;
+        width: 100%;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         border: none;
+
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+
+    dialog p {
+        text-align: center;
     }
 
     .loadingCircle {
         border: 2.5px solid #f3f3f3; /* Light grey */
-        border-top: 2.5px solid yellow;
+        border-top: 2.5px solid #FFD460;
         border-radius: 50%;
         width: 40px;
         height: 40px;
